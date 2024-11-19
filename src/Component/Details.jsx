51c23ea33965fde6497651d3d2b391fb39c7e9e0
johhny47 <mdjohnny063@gmail.com>
2024-11-19 19:08:06 +0600

    add feedback

--- src/Component/Details.jsx
+++ src/Component/Details.jsx
@@ -1,9 +1,29 @@
 import { useEffect } from "react";
 import { useLoaderData } from "react-router-dom";
+import React, { useState } from 'react';
+
+
 
 
 const Details = () => {
      const  data = useLoaderData()
+     
+
+
+     const [text, setText] = useState('');
+    
+     const [showText, setShowText] = useState('');
+   
+     const handleInputChange = (event) => {
+       setText(event.target.value);
+      
+     };
+   const handleButtonClick = () => {
+    setShowText(text);
+       setText('');
+     };
+   
+
    
     const {id,service_name,category,brief_description,pricing,image,counselor} = data
     useEffect(()=>{
@@ -12,16 +32,17 @@ const Details = () => {
     return (  
        
       
-      <div className="h-[500px] w-[1062px] flex gap-10 my-20">
-         <div className=" w-[35%]">
+     <div className="  w-[1062px] mx-auto">
+       <div className=" mx-auto md:flex lg:flex gap-10 my-10 lg:mb-0">
+         <div className=" w-[30%] md:w-[45%] lg:w-[45%] ">
             <img src={image} className="rounded-xl" alt="" />
          </div>
-         <div className=" w-[60%] ">
+         <div className=" w-[45%]">
            <h1 className="text-3xl font-bold ">{service_name}</h1>
-           <h2 className="text-xl font-medium my-2">Price: ${pricing}</h2>
-           <div className="badge badge-outline border-green-400 bg-green-200 text-green-500 font-medium ">In Stock</div>
+           <h2 className="text-xl font-medium my-2">Price: {pricing}</h2>
+          
            <h2 className="text-xl font-medium mt-2">Description</h2>
-           <h2>{brief_description}</h2>
+           <h2 className="w-48">{brief_description}</h2>
            <h2 className="text-xl font-medium mt-3">Counselor</h2>
            <h2>{counselor}</h2>
            
@@ -37,8 +58,21 @@ const Details = () => {
       
        
          </div>
+         
+        
       </div>
-      
+      <div className="mb-6">
+            <h1 className="font-bold">Feedback</h1>
+            <textarea className="textarea textarea-bordered" cols={35} rows={5} placeholder="Share your feedback"  value={text} 
+        onChange={handleInputChange} 
+       ></textarea>
+           <div>
+           <button  onClick={handleButtonClick} className="btn btn-primary inline-block">Submit</button>
+           </div>
+         </div>
+         <h1>Show Feedback: {showText}</h1>
+        
+     </div>
     
    
       
